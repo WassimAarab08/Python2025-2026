98fe88483398a87bffc19c35916c36857aff4468
wassi <wassimaarab18@gmail>
2025-09-23 19:43:54 +0200

    v2

---
+++ Practicas/0032.py
@@ -0,0 +1,8 @@
+lista=[]
+
+for i in range(1,11):
+    num = int(input("Ingresa un numero para añadir a la lista : "))
+    lista.append(num)
+
+print(f"El primer elemento es {lista[0]}")
+print(f"El ultimo elemento es {lista[-1]}")
---
+++ Practicas/0033.py
@@ -0,0 +1,4 @@
+semana = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+
+print("El primer dia es  => ", semana[0])
+print("El ultimo dia es  => ", semana[-1])
